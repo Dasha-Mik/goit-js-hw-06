014f279de3cf5442b820aa73eb3087e779f6f9a6
Dasha-Mik <Dashamikhaik@gmail.com>
2022-02-15 21:11:31 +0200

    new commit

--- js/task-06.js
+++ js/task-06.js
@@ -3,14 +3,7 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', onInputBlur);
  
 function onInputBlur() {
-  console.log(input.value.length);
-  console.log(input.getAttribute('data-length'));
-  console.log(input.value);
-  if (input.value.length !== Number(input.getAttribute('data-length'))) {
-    input.classList.add('invalid');
-    input.classList.remove('valid');
-  } else {
-    input.classList.remove('invalid');
-    input.classList.add('valid');
-  }
+  const hasCorectLength = input.value.length === +input.dataset.length;
+  input.classList.toggle('invalid', !hasCorectLength);
+  input.classList.toggle('valid', hasCorectLength);
 };
--- js/task-09.js
+++ js/task-09.js
@@ -4,8 +4,9 @@ const btnColor = document.querySelector(".change-color");
 const spanColor = document.querySelector(".color");
 
 function getRandomHexColor() {
-    document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    spanColor.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    document.body.style.backgroundColor = color;
+    spanColor.textContent = color;
 }
 
 
